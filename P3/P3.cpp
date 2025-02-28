e378bb28579448ef34c62213c0b6d67d76754ea9
YeeTing18 <yeetingong2003@gmail.com>
2025-02-28 10:03:46 +0800

    28/2 Practical

--- P3/P3.cpp
+++ P3/P3.cpp
@@ -58,7 +58,7 @@ void private_data_exercise3() {
 
 static long num_steps = 100000000;
 double step;
-int compute_pi()
+double compute_pi()
 {
     int i;
     double x, pi, sum = 0.0;
@@ -66,50 +66,98 @@ int compute_pi()
 
     step = 1.0 / (double)num_steps;
 
-
     start_time = omp_get_wtime();
 
-    for (i = 1;i <= num_steps; i++) {
+
+    for (i = 1; i <= num_steps; i++) {
         x = (i - 0.5) * step;
         sum = sum + 4.0 / (1.0 + x * x);
     }
 
+
     pi = step * sum;
     run_time = omp_get_wtime() - start_time;
     printf("\n pi with %ld steps is %.16lf in %lf seconds\n ", num_steps, pi, run_time);
-    return 0;
+
+    return run_time;
 }
 
-int compute_pi_in_parallel()
+double compute_pi_in_parallel_v1()
 {
     double pi, sum = 0.0;
     double start_time, run_time;
     const int THREAD_SIZE = 16;
-    double partial_sum[THREAD_SIZE] = {0.0};
+    double partial_sum[THREAD_SIZE] = { 0.0 };
 
     step = 1.0 / (double)num_steps;
     start_time = omp_get_wtime();
 
-    #pragma omp parallel num_threads(THREAD_SIZE)
+#pragma omp parallel num_threads(THREAD_SIZE)
     {
-        int id = omp_get_thread_num();
         int i;
         double x;
-        for (i = id + 1;i <= num_steps; i +=THREAD_SIZE) {
+        int id = omp_get_thread_num();
+        for (i = id + 1; i <= num_steps; i += THREAD_SIZE) {
             x = (i - 0.5) * step;
-            partial_sum[id] += 4.0 / (1.0 + x * x); 
+            partial_sum[id] += 4 / (1.0 + x * x);
         }
     }
-    for (int i = 0; i < THREAD_SIZE; i++) {
-        //partial_sum[i] = 0.0;
-        sum += partial_sum[i];
+
+    for (int j = 0; j < THREAD_SIZE; j++) {
+        sum += partial_sum[j];
     }
 
     pi = step * sum;
     run_time = omp_get_wtime() - start_time;
     printf("\n pi with %ld steps is %.16lf in %lf seconds\n ", num_steps, pi, run_time);
-    return 0;
+
+    return run_time;
 }
+
+double compute_pi_in_parallel_v2()
+{
+    double pi, sum = 0.0;
+    double start_time, run_time;
+    const int THREAD_SIZE = 16;
+    const int cache = 64;
+    double partial_sum[THREAD_SIZE][cache];
+
+    double step = 1.0 / (double)num_steps;  // Step size
+
+    start_time = omp_get_wtime();
+
+    //for loop to initialize all partial_sum elements to zero
+    for (int i = 0; i < THREAD_SIZE; i++) {
+        for (int j = 0; j < cache; j++) {
+            partial_sum[i][j] = 0.0;
+        }
+    }
+
+#pragma omp parallel num_threads(THREAD_SIZE)
+    {
+        int i;
+        double x;
+        int id = omp_get_thread_num();
+        for (i = id + 1; i <= num_steps; i += THREAD_SIZE) {
+            x = (i - 0.5) * step;
+            partial_sum[id][i % cache] += 4 / (1.0 + x * x);
+        }
+    }
+
+    //for loop to total up all partial sum
+    for (int j = 0; j < THREAD_SIZE; j++) {
+        for (int i = 0; i < cache; i++) {
+            sum += partial_sum[j][i];  
+        }
+    }
+
+    pi = step * sum;
+    run_time = omp_get_wtime() - start_time;
+    printf("\n pi with %ld steps is %.16lf in %lf seconds\n ", num_steps, pi, run_time);
+
+    return run_time;
+}
+
 int main()
 {
     //thread_id_exercise();
@@ -117,8 +165,19 @@ int main()
     //private_data_exercise1();
     //private_data_exercise2();
     //private_data_exercise3();
-    compute_pi();
-    compute_pi_in_parallel();
+    //compute_pi();
+    //compute_pi_in_parallel_v1();
+    //compute_pi_in_parallel_v2();
+
+
+    double rt[3];
+	rt[0] = compute_pi();
+	rt[1] = compute_pi_in_parallel_v1();
+	rt[2] = compute_pi_in_parallel_v2();
+
+    cout << endl;
+	cout << "The performance gain of rt[1] is " << rt[0] / rt[1] << endl;
+	cout << "The performance gain of rt[2] is " << rt[0] / rt[2] << endl;
 
 
     //int age = 23;
